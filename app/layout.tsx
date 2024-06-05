322560a71a904cbcdd362b0e921fd662b9b3420d
Andy Santana <andysantana0106@gmail.com>
2024-06-05 11:46:47 -0400

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter as FontSans, JetBrains_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         <AppProviders>{children}</AppProviders>
       </body>
+      <Analytics />
     </html>
   );
 }
