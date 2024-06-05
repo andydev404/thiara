55835d1c00c5a6150c81dbacc456bdf005e5dc10
Andy Santana <andysantana0106@gmail.com>
2024-06-05 11:53:59 -0400

    feat: add demo link to README

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 Thiara makes it easy to manage and organize your prompts, and it allows you to create an endless number of personalized prompts.
 
+[GO TO DEMO](https://thiara.vercel.app)
+
 ![Alt text](https://i.ibb.co/DwDBhCQ/home.jpg "HomePage")
 ![Alt text](https://i.ibb.co/DQXKc6n/dashboard.jpg "Dashboard")
--- components/landing/features.tsx
+++ components/landing/features.tsx
@@ -57,7 +57,7 @@ export default function Features() {
             </div>
           </div>
           <Image
-            src="/thiara.jpeg"
+            src="/Thiara.jpeg"
             alt="Product"
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
